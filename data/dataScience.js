a9e4df76e3b4ecc1eccae81e70f449cf5aa2a70c
Elir-Mahad <mmahad280@gmail.com>
2023-09-28 18:07:51 -0400

    feat(data): add ds data test

---
+++ data/dataScience.js
@@ -0,0 +1,124 @@
+export const jobData = [
+  //   1. Data Analyst:
+  // Career Track: Junior Data Analyst ➔ Data Analyst ➔ Senior Data Analyst ➔ Lead Data Analyst ➔ Analytics Manager
+
+  // 2. Business Analyst:
+  // Career Track: Junior Business Analyst ➔ Business Analyst ➔ Senior Business Analyst ➔ Business Intelligence (BI) Manager ➔ Director of Business Analytics
+
+  // 3. Data Scientist:
+  // Career Track: Junior Data Scientist ➔ Data Scientist ➔ Senior Data Scientist ➔ Machine Learning Engineer ➔ Chief Data Scientist
+
+  // 4. Machine Learning Engineer:
+  // Career Track: Junior Machine Learning Engineer ➔ Machine Learning Engineer ➔ Senior Machine Learning Engineer ➔ Machine Learning Architect ➔ Chief AI Officer
+
+  // 5. Data Engineer:
+  // Career Track: Junior Data Engineer ➔ Data Engineer ➔ Senior Data Engineer ➔ Data Engineering Manager ➔ Chief Data Officer
+
+  // 6. Data Analyst (Business Intelligence):
+  // Career Track: Junior BI Analyst ➔ BI Analyst ➔ Senior BI Analyst ➔ BI Manager ➔ Director of Business Intelligence
+
+  // 7. Statistician:
+  // Career Track: Junior Statistician ➔ Statistician ➔ Senior Statistician ➔ Principal Statistician ➔ Chief Statistician
+
+  // 8. Research Scientist (AI/ML):
+  // Career Track: Junior Research Scientist ➔ Research Scientist ➔ Senior Research Scientist ➔ Principal Research Scientist ➔ Chief AI Research Officer
+
+  // 9. Chief Data Officer (CDO):
+  // Career Track: Data Analyst/Data Scientist ➔ Data Team Lead/Manager ➔ Director of Data Strategy ➔ Chief Data Officer
+  {
+    jobCategory: "Data Analyst",
+    jobLevels: [
+      {
+        level: "Junior Data Analyst",
+        jobDescription:
+          "Entry-level role focusing on data collection, cleaning, and basic analysis. Collaborates with senior analysts to create reports and visualizations.",
+        requiredSkills: ["Data Collection", "Data Cleaning", "Basic Analysis"],
+        responsibilities: [
+          "Assist in data collection and cleaning",
+          "Collaborate with the team on analysis projects",
+          "Create basic data visualizations",
+        ],
+        experience: "0-2 years of relevant experience",
+        certifications: [],
+        potentialCompensationToronto: "$45,000 - $65,000 CAD per year",
+        potentialCompensationCanada: "$40,000 - $60,000 CAD per year",
+      },
+      {
+        level: "Data Analyst",
+        jobDescription:
+          "Collects and analyzes data, generates reports, and assists in decision-making. May work independently on smaller projects.",
+        requiredSkills: [
+          "Data Analysis",
+          "Report Generation",
+          "Decision Support",
+        ],
+        responsibilities: [
+          "Collect and analyze data for insights",
+          "Generate reports and data visualizations",
+          "Contribute to decision-making processes",
+        ],
+        experience: "2-4 years of relevant experience",
+        certifications: ["Data Analyst Certification"],
+        potentialCompensationToronto: "$60,000 - $85,000 CAD per year",
+        potentialCompensationCanada: "$50,000 - $80,000 CAD per year",
+      },
+      {
+        level: "Senior Data Analyst",
+        jobDescription:
+          "Leads data analysis projects, provides insights, and mentors junior analysts.",
+        requiredSkills: [
+          "Advanced Data Analysis",
+          "Project Leadership",
+          "Mentoring",
+        ],
+        responsibilities: [
+          "Lead data analysis projects",
+          "Provide advanced insights",
+          "Mentor junior analysts",
+        ],
+        experience: "4-6 years of relevant experience",
+        certifications: ["Advanced Data Analyst Certification"],
+        potentialCompensationToronto: "$80,000 - $110,000 CAD per year",
+        potentialCompensationCanada: "$70,000 - $100,000 CAD per year",
+      },
+      {
+        level: "Lead Data Analyst",
+        jobDescription:
+          "Manages data analysis teams, sets analysis strategies, and oversees complex projects.",
+        requiredSkills: [
+          "Team Leadership",
+          "Strategy Development",
+          "Project Management",
+        ],
+        responsibilities: [
+          "Manage data analysis teams",
+          "Set analysis strategies",
+          "Oversee complex projects",
+        ],
+        experience: "6+ years of relevant experience",
+        certifications: ["Leadership Certification"],
+        potentialCompensationToronto: "$100,000 - $140,000 CAD per year",
+        potentialCompensationCanada: "$90,000 - $130,000 CAD per year",
+      },
+      {
+        level: "Analytics Manager",
+        jobDescription:
+          "Directs the analytics department, shapes the organization's data-driven strategy, and leads data-driven initiatives.",
+        requiredSkills: [
+          "Department Leadership",
+          "Data Strategy",
+          "Initiative Leadership",
+        ],
+        responsibilities: [
+          "Direct the analytics department",
+          "Shape the organization's data-driven strategy",
+          "Lead data-driven initiatives",
+        ],
+        experience: "8+ years of relevant experience",
+        certifications: ["Certified Analytics Director (CAD)"],
+        potentialCompensationToronto: "$120,000 - $180,000+ CAD per year",
+        potentialCompensationCanada: "$110,000 - $170,000+ CAD per year",
+      },
+    ],
+  },
+];
