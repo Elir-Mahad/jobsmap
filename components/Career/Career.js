7940b3e8621d4ab15f02836365bb1f3e3caac0b3
Elir-Mahad <mmahad280@gmail.com>
2023-10-11 13:01:21 -0400

    enhance(career): toogle career track jobs

--- components/Career/Career.js
+++ components/Career/Career.js
@@ -1,15 +1,22 @@
+import React, { useState } from "react";
 import Job from "../Job/Job";
 import style from "./Career.module.css";
 
 function Career({ category }) {
+  const [isCareerTrack, setIsCareerTrack] = useState(false);
+  const toggleCareerTrack = () => {
+    setIsCareerTrack(!isCareerTrack);
+  };
   return (
     <div key={category.jobCategory} className={style.jobCat}>
-      <h2>{category.jobCategory}</h2>
-      <div>
-        {category.jobLevels.map((level, index) => (
-          <Job level={level} index={index} />
-        ))}
-      </div>
+      <h2 onClick={toggleCareerTrack}>{category.jobCategory}</h2>
+      {isCareerTrack && (
+        <div>
+          {category.jobLevels.map((level, index) => (
+            <Job level={level} index={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
